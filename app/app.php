aeb470d1754e71430c1faacbd8d7472bc230ed30
Josh Overly and Jon Sweeney <joshoverly@yahoo.com>
2016-02-10 16:56:47 -0800

    add feed function

--- app/app.php
+++ app/app.php
@@ -24,6 +24,15 @@
         return $app['twig']->render('add_gotchi.html.twig', array('newgotchi' => $gotchi));
     });
 
+    $app->get("/feed", function() use ($app) {
+        $current_gotchi = Gotchi::getAll();
+        $current_gotchi[0]->setFood(0);
+        $food = $current_gotchi[0]->getFood();
+        $current_gotchi[0]->setFood($food + 1);
+
+        return $app['twig']->render('feed.html.twig');
+    });
+
     $app->post("/delete_gotchis", function() use($app) {
         Gotchi::deleteAll();
         return $app['twig']->render('delete_gotchis.html.twig');
--- src/Gotchi.php
+++ src/Gotchi.php
@@ -3,20 +3,40 @@ class Gotchi
 {
     /****Properties****/
     private $name;
+    private $food;
+    private $sleep;
     /****Constructor****/
     function __construct($name)
     {
         $this->name = $name;
+        $this->food = $food = 5;
+        $this->sleep = $sleep = 5;
     }
     /****Setters****/
     function setName($new_name)
     {
         $this->name = (string) $new_name;
     }
+    function setFood($new_food)
+    {
+        $this->food = (integer) $new_food;
+    }
+    function setSleep($new_sleep)
+    {
+        $this->sleep = (integer) $new_sleep;
+    }
     /****Getters****/
     function getName()
     {
         return $this->name;
+    }
+    function getFood()
+    {
+        return $this->food;
+    }
+    function getSleep()
+    {
+        return $this->sleep;
     }
     /****Functions****/
     //save all
